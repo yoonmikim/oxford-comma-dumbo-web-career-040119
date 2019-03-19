f4a59c7ede0fb09acccc71471bf9cb5a9d8def5f
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-19 20:38:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if (array.length == 1)
+    array.join(", ")
+  else if (array.length == 2)
+    array.join(" and ")
+  else
+    array[0...-1].join(", ") << " and #{array[-1]}"
+  end
 end
